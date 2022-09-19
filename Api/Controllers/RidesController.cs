e4d53afa03a5301b8ee705d7ab2612a7916050c4
Alexandros Peppas <apeppas@microsoft.com>
2022-09-19 11:56:12 +0100

    cleaned project structure

---
+++ Api/Controllers/RidesController.cs
@@ -0,0 +1,27 @@
+using GotSpaceSolution.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GotSpaceSolution.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RidesController : ControllerBase
+    {
+        private readonly ILogger<RidesController> logger;
+        private readonly IRideRouteService rideRouteService;
+
+        public RidesController(ILogger<RidesController> logger, IRideRouteService rideRouteService)
+        {
+            this.logger = logger;
+            this.rideRouteService = rideRouteService;
+        }
+
+        [HttpPost(Name = "CreateRouteRide")]
+        public async Task<IActionResult> CreateRouteRide (RideRouteEntity entity, CancellationToken cancellationToken = default)
+        {
+            await rideRouteService.CreateNewRideRouteAsync(entity, cancellationToken);
+
+            return Ok();
+        }
+    }
+}
