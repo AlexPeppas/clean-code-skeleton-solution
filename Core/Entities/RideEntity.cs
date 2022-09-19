058f38877c5e0c7a124735ed7c5be2162042301a
Tony Lim <tonylim@microsoft.com>
2022-09-19 17:19:39 +0100

    Adds Booking and User entities, adds attributes to Ride entity and adds creating Booking contract

---
+++ Core/Entities/RideEntity.cs
@@ -0,0 +1,22 @@
+﻿using GotSpaceSolution.Common;
+
+namespace GotSpaceSolution.Core
+{
+    public sealed class RideEntity : BaseEntity
+    {
+        public Guid RideId { get; set; }
+
+        public UserEntity CreatedBy { get; set; } 
+
+        public int NumberOfSeats { get; set; }
+
+        public Boolean OnlyWoman { get; set; }
+
+        public string SourceName { get; set; }
+
+        public string DestinationName { get; set; } 
+
+        public string Status { get; set; }
+        
+    }
+}
